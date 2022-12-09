32c8acb9dca45a9b858f92008538397634f48b63
samikia <kia.sami8@gmail.com>
2022-12-09 15:05:35 +0330

    feat: add people

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,27 @@
+import { useState } from "react";
+import AddPeople from "./components/addPeople";
+import List from "./components/list";
+export interface IPeople{
+  id?:number;
+  fullName?:string;
+  age?:number;
+  img_url?:string;
+  bio?:string;
 
+}
 const App = ()=> {
+  const[people,setPeople]=useState<IPeople[]>([{
+    id:1,
+    fullName:"samira Kian",
+    age:35,
+    img_url:"https://media.istockphoto.com/id/1382459668/ro/fotografie/femeie-destul-de-t%C3%A2n%C4%83r%C4%83-afro-printre-palmieri.jpg?s=612x612&w=is&k=20&c=ThPaw5nLn_qu9bUugdFg3PLEz2sJ72rcj9A3GfVDSBo=",
+    bio:"frontend developer"
+  }]);
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <h4 className="alert alert-info">People management</h4>
+    <List people={people} />
+    <AddPeople people={people} setPeople={setPeople}/>
     </div>
   );
 }
